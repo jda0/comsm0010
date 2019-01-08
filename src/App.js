3f7473f6477e87db941e63f8581a5fb42bb4ed86
James Daly <da_j@live.co.uk>
2019-01-08 16:22:38 +0000

    Add event pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+import Event from './components/Event'
+
+const AUTH_SERVER = 'https://takon.auth.eu-west-1.amazoncognito.com/'
+
+const CLIENT_ID = 'sgkghjjfsd9gfkg06qe1par1v'
+const REDIRECT_URI = 'https%3A%2F%2Ftakon.me%2F%3Fcallback'
+const AUTH_QUERY = `?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {}
+
+    let hashes = window.location.hash.slice(1).split('&').map(x => x.split('='))
+    for (const h of hashes) if (h[0] === 'id_token') this.state.id_token = h[1]
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div className='App container'>
+          <header className='blog-header py-3'>
+            <div className='row flex-nowrap justify-content-between align-items-center'>
+              <div className='col-4 pt-1'>
+                <span className='text-muted'>The Tickets Exchange</span>
+              </div>
+              <div className='col-4 text-center'>
+                <Link className='blog-header-logo text-dark' to='/'>takon.me</Link>
+              </div>
+              <div className='col-4 d-flex justify-content-end align-items-center'>
+                {/* <a className='text-muted' href='#'>
+                <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' className='mx-3' focusable='false' role='img'><title>Search</title><circle cx='10.5' cy='10.5' r='7.5' /><line x1='21' y1='21' x2='15.8' y2='15.8' /></svg>
+              </a> */}
+                {
+                  !this.state.id_token && (
+                    <span>
+                      <a className='btn btn-sm btn-link text-muted mr-1' href={AUTH_SERVER + 'login' + AUTH_QUERY}>Login</a>
+                      <a className='btn btn-sm btn-outline-secondary' href={AUTH_SERVER + 'signup' + AUTH_QUERY}>Sign up</a>
+                    </span>
+                  )
+                }
+                {
+                  this.state.id_token && (
+                    <span className='text-muted'>{this.state.id_token.slice(0, 12)}</span>
+                  )
+                }
+
+              </div>
+            </div>
+          </header>
+          <main>
+            <Route exact path='/' component={Home} />
+            <Route path='/events/:id' component={Event} />
+          </main>
+          { this.state.id_token && (<span>Authorization: Bearer {this.state.id_token}</span>) }
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
