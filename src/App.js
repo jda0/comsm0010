298a4da9b2b9a16c99a834b901c92a1dfded7e28
James Daly <da_j@live.co.uk>
2019-01-10 15:51:52 +0000

    Add all screens and fix nav

--- src/App.js
+++ src/App.js
@@ -1,55 +1,79 @@
 import React, { Component } from 'react'
+import { withCookies } from 'react-cookie'
 import {
   BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect,
-  withRouter
+  Route
 } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
 import Home from './components/Home'
+import CreateEvent from './components/CreateEvent'
+import CreateOffer from './components/CreateOffer'
 import Event from './components/Event'
-
-const AUTH_SERVER = 'https://takon.auth.eu-west-1.amazoncognito.com/'
-
-const CLIENT_ID = 'sgkghjjfsd9gfkg06qe1par1v'
-const REDIRECT_URI = 'https%3A%2F%2Ftakon.me%2F%3Fcallback'
-const LOGOUT_URI = 'https%3A%2F%2Ftakon.me%2F%3Flogout'
-const AUTH_QUERY = `?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`
-const LOGOUT_ENDPOINT = `logout?client_id=${CLIENT_ID}&logout_uri=${LOGOUT_URI}`
-
-const FETCH_PARAMS = {
-  method: 'GET',
-  mode: 'cors',
-  headers: { 'x-api-key': 'wysAX36RWg5p0EJJdxi9j92DMinJ9Vl04HQEZ10Z' }
-}
+import Topup from './components/Topup'
 
 class App extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      id_token: props.cookies.get('id_token') || ''
+    }
+
+    this.AUTH_SERVER = 'https://takon.auth.eu-west-1.amazoncognito.com/'
+    this.CLIENT_ID = 'sgkghjjfsd9gfkg06qe1par1v'
+    this.REDIRECT_URI = 'https%3A%2F%2Ftakon.me%2F%3Fcallback'
+    this.LOGOUT_URI = 'https%3A%2F%2Ftakon.me%2F%3Flogout'
+    this.AUTH_QUERY = `?response_type=token&client_id=${this.CLIENT_ID}&redirect_uri=${this.REDIRECT_URI}`
+    this.LOGOUT_ENDPOINT = `logout?client_id=${this.CLIENT_ID}&logout_uri=${this.LOGOUT_URI}`
+    this.FETCH_PARAMS = {
+      method: 'GET',
+      mode: 'cors',
+      headers: { 'x-api-key': 'wysAX36RWg5p0EJJdxi9j92DMinJ9Vl04HQEZ10Z' }
+    }
 
     let hashes = window.location.hash.slice(1).split('&').map(x => x.split('='))
-    for (const h of hashes) if (h[0] === 'id_token') this.state.id_token = h[1]
+    for (const h of hashes) {
+      switch (h[0]) {
+        case 'id_token':
+          this.state.id_token = h[1]
+          this.props.cookies.set('id_token', h[1], {
+            maxAge: 3300,
+            path: '/'
+          })
+          break
+        case 'state': this.state.urlState = h[1]; break
+        default: break
+      }
+    }
+    window.location.hash = '#'
   }
 
   componentDidMount () {
     if (this.state.id_token) {
       // @ts-ignore
       fetch('https://eozp8bius7.execute-api.eu-west-1.amazonaws.com/test/users/me', {
-        ...FETCH_PARAMS,
+        ...this.FETCH_PARAMS,
         headers: {
-          ...FETCH_PARAMS.headers,
+          ...this.FETCH_PARAMS.headers,
           'Authorization': `Bearer ${this.state.id_token}`
         }
       })
         .then(x => x.json().then(x => {
           console.log(x)
           this.setState({
-            user: x
+            user: {
+              funds: 0,
+              ...x
+            }
           })
         }))
-        .catch(x => console.error('error', x))
+        .catch(x => {
+          console.error('error', x)
+          this.props.cookies.remove('id_token', {
+            path: '/'
+          })
+          this.setState({ id_token: undefined })
+        })
     }
   }
 
@@ -57,53 +81,19 @@ class App extends Component {
     return (
       <Router>
         <div className='App container'>
-          <header className='blog-header py-3'>
-            <div className='row flex-nowrap justify-content-between align-items-center'>
-              <div className='col-4 pt-1'>
-                <span className='text-muted'>The Tickets Exchange</span>
-              </div>
-              <div className='col-4 text-center'>
-                <Link className='blog-header-logo text-dark' to='/'>takon.me</Link>
-              </div>
-              <div className='col-4 d-flex justify-content-end align-items-center'>
-                {/* <a className='text-muted' href='#'>
-                <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' className='mx-3' focusable='false' role='img'><title>Search</title><circle cx='10.5' cy='10.5' r='7.5' /><line x1='21' y1='21' x2='15.8' y2='15.8' /></svg>
-              </a> */}
-                {
-                  !this.state.id_token && (
-                    <span>
-                      <a className='btn btn-sm btn-link text-muted mr-1' href={AUTH_SERVER + 'login' + AUTH_QUERY}>Login</a>
-                      <a className='btn btn-sm btn-outline-secondary' href={AUTH_SERVER + 'signup' + AUTH_QUERY}>Sign up</a>
-                    </span>
-                  )
-                }
-                {
-                  this.state.id_token && (
-                    <div className='btn-group'>
-                      <button type='button' className='btn btn-sm btn-link dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                        {(this.state.user && this.state.user.user) || '🧑'}
-                      </button>
-                      <div className='dropdown-menu dropdown-menu-right'>
-                        <Link className='dropdown-item' to='/create/event'>Create Event</Link>
-                        <Link className='dropdown-item' to='/me/funds'>Add Funds&ensp;<span className='text-muted'>£50.00</span></Link>
-                        <div className='dropdown-divider' />
-                        <a className='dropdown-item' href={AUTH_SERVER + LOGOUT_ENDPOINT}>Log out</a>
-                      </div>
-                    </div>
-                  )
-                }
-              </div>
-            </div>
-          </header>
+          <Route render={props => <Header {...props} app={this} />} />
           <main>
             <Route exact path='/' render={props => <Home {...props} app={this} />} />
-            <Route path='/events/:id' component={Event} />
+            <Route exact path='/topup' render={props => <Topup {...props} app={this} />} />
+            <Route exact path='/create/event' render={props => <CreateEvent {...props} app={this} />} />
+            <Route exact path='/events/:id' component={Event} />
+            <Route exact path='/events/:id/ask' render={props => <CreateOffer {...props} app={this} type='ask' />} />
+            <Route exact path='/events/:id/bid' render={props => <CreateOffer {...props} app={this} type='bid' />} />
           </main>
-          { this.state.id_token && (<span>Authorization: Bearer {this.state.id_token}</span>) }
         </div>
       </Router>
     )
   }
 }
 
-export default App
+export default withCookies(App)
---
+++ src/components/CreateOffer.js
@@ -0,0 +1,176 @@
+import React, { Component } from 'react'
+import { DateTime as DT } from 'luxon'
+
+class CreateOffer extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      eventid: props.match.params.id
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleReserve = this.handleReserve.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount () {
+    fetch(`https://eozp8bius7.execute-api.eu-west-1.amazonaws.com/test/events/${this.state.eventid}`, this.props.app.FETCH_PARAMS)
+      .then(x => x.json().then(x => {
+        // console.log(x)
+        this.setState({
+          event: x,
+          askAmount: 50,
+          bidAmount: 50
+        })
+      }))
+      .catch(x => console.error('error', x))
+  }
+
+  handleChange (event) {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
+    this.setState({ [event.target.id]: value })
+  }
+
+  handleReserve (event) {
+    console.log('reserve')
+    this.setState({ reserved: {} })
+    const afterTimeout = () => {
+      this.setState({
+        reserved: {
+          pending: false,
+          id: 1,
+          price: 49
+        },
+        priceInput: 49
+      })
+
+      if (this.props.type === 'bid') {
+        this.setState({
+          ticketIdInput: 1
+        })
+      }
+    }
+    setTimeout(afterTimeout, 2000)
+    event.preventDefault()
+    event.stopPropagation()
+  }
+
+  handleSubmit (event) {
+    // if (this.state.tcsCheck) {
+    //   fetch(`https://eozp8bius7.execute-api.eu-west-1.amazonaws.com/test/events`, {
+    //     ...this.props.app.FETCH_PARAMS,
+    //     method: 'POST',
+    //     headers: {
+    //       ...this.props.app.FETCH_PARAMS.headers,
+    //       'Authorization': `Bearer ${this.props.app.state.id_token}`
+    //     }
+    //   })
+    //     .then(x => x.json().then(x => {
+    //       console.log(x)
+    //       this.setState({
+    //         event: x.Item,
+    //         askAmount: 50,
+    //         bidAmount: 50
+    //       })
+    //     }))
+    //     .catch(x => console.error('error', x))
+    // }
+
+    event.preventDefault()
+  }
+
+  render () {
+    return (
+      <div>
+        <h1 className='text-center my-5'>Create {/[aeiou]/i.test(this.props.type[0]) ? 'an' : 'a'} {this.props.type.toTitleCase()}</h1>
+        { (!this.props.app.state.user && (
+          <div className='alert alert-secondary'>
+            You need to <a href={this.props.app.AUTH_SERVER + 'login' + this.props.app.AUTH_QUERY + `&state=${this.props.location.pathname}`}>Login</a> or <a href={this.props.app.AUTH_SERVER + 'signup' + this.props.app.AUTH_QUERY + `&state=${this.props.location.pathname}`}>Sign up</a> to create an event.
+          </div>
+        )) || (
+          <form onSubmit={this.handleSubmit}>
+            <div className='form-group row'>
+              <label className='col-sm-3 col-form-label'>Event</label>
+              <div className='col-sm-auto mb-3'>
+                <div className='card flex-md-row shadow-sm'>
+                  {(this.state.event && (
+                    <div className='card-body d-flex flex-column align-items-start'>
+                      <h3 className='mb-0'>{(this.state.event.title && this.state.event.title.toTitleCase()) || '???'}</h3>
+                      {this.state.event.subtitle && (<h5 className='mb-2'>{this.state.event.subtitle.toTitleCase() || ''}</h5>)}
+                      <p className='card-text mb-auto'>
+                        <span className='text-muted mr-3 no-wrap'>{this.state.event.location && this.state.event.location.toTitleCase()}</span>
+                        <wbr />
+                        <span className='text-muted no-wrap'>{this.state.event.datetime && DT.fromSeconds(this.state.event.datetime).toLocaleString(DT.DATETIME_MED)}</span>
+                      </p>
+                    </div>
+                  )) || (<div className='spinner' />)}
+                </div>
+              </div>
+            </div>
+            {(this.state.reserved && !this.state.reserved.id && (
+              <div className='row'>
+                <div className='col-sm-9 offset-sm-3'>
+                  <div className='spinner dark' />
+                </div>
+              </div>
+            )) || (
+              <div>
+                {(this.props.type.toLowerCase() !== 'bid' || this.state.reserved) && (
+                  <div className='form-group row'>
+                    <label htmlFor='ticketIdInput' className='col-sm-3 col-form-label'>Ticket ID</label>
+                    <div className='col mb-1'>
+                      <input type='text' disabled={this.props.type.toLowerCase() === 'bid'} className='form-control' id='ticketIdInput' placeholder='TICKET0001' value={this.state.ticketIdInput || ''} onChange={this.handleChange} required />
+                    </div>
+                  </div>
+                )}
+                <div className='form-group row'>
+                  <label htmlFor='subtitleInput' className='col-sm-3 col-form-label'>{this.props.type.toTitleCase()}</label>
+                  <div className='col'>
+                    <div className='input-group mb-1 mr-sm-1'>
+                      <div className='input-group-prepend'>
+                        <div className='input-group-text'>£</div>
+                      </div>
+                      <input type='number' className='form-control' id='priceInput' step='0.01' placeholder='71.50' value={this.state.priceInput || ''} onChange={this.handleChange} required />
+                    </div>
+                  </div>
+                  {!this.state.reserved && (<div className='col-12 col-sm-auto mb-1'>
+                    <button className='btn btn-success' onClick={this.handleReserve}>Reserve Best Price</button>
+                  </div>)}
+                </div>
+                <div className='form-group row form-check my-4'>
+                  <div className='col-sm-9 offset-sm-3 custom-control custom-checkbox'>
+                    <input type='checkbox' className='custom-control-input' id='tcsCheck' checked={this.state.tcsCheck} onChange={this.handleChange} required />
+                    <label className='custom-control-label' htmlFor='tcsCheck'>
+                      I agree to the Terms and Conditions and Privacy Policy.&nbsp;
+
+                      { (parseFloat(this.state.priceInput) > 0) && (
+                        <span className='text-muted'>
+                        Once you click create, your {this.props.type.toLowerCase()} will
+                        immediately be added to the market and you'll be debited £{this.props.type.toLowerCase() === 'bid' ? (parseFloat(this.state.priceInput, 2) || 71.50).toFixed(2) : `${((parseFloat(this.state.priceInput, 2) || 71.50) * 0.15).toFixed(2)}. This is necessary to prevent fraud on our platform and will be either refunded when your ask is settled, used for cancellation fees or confiscated if your ask is found to be fraudulent`}.
+                        Cancellation of this {this.props.type.toLowerCase()} is subject to a fee of
+                        £{((this.state.priceInput || 71.50) * 0.15).toFixed(2)}, and
+                        is not possible once fulfilled by a
+                        corresponding {this.props.type.toLowerCase() === 'ask' ? 'bid' : 'ask'}.
+                        The {this.props.type.toLowerCase()} may be fulfilled immediately
+                        if a corresponding {this.props.type.toLowerCase() === 'ask' ? 'bid' : 'ask'} already
+                        exists. {this.props.type.toLowerCase() === 'ask' && `You'll receive £${((parseFloat(this.state.priceInput, 2) || 71.50) * 1.05).toFixed(2)} after processing fees when your ask is fulfilled, inclusive of your refund of £${((this.state.priceInput || 71.50) * 0.15).toFixed(2)}.`}
+                        </span>
+                      )}
+                    </label>
+                  </div>
+                </div>
+                <div className='form-group row justify-content-end'>
+                  <div className='col-auto'>
+                    <button type='submit' className='btn btn-primary'>Create</button>
+                  </div>
+                </div>
+              </div>
+            )}
+
+          </form>
+        ) }
+      </div>
+    )
+  }
+}
+
+export default CreateOffer
