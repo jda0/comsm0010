6954c9bddd5e3f011b89277a95c7ddfa4e8e7215
James D <da_j@live.co.uk>
2019-01-13 19:48:06 +0000

    Add 404

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound () {
+  return (
+    <div>
+      <h1 className='display-1 my-5 text-center'>:(</h1>
+      <h2 className='text-center'>What the Fuck, Richard</h2>
+      <h4 className='text-center'>I can't believe you've done this</h4>
+    </div>
+  )
+}
+
+export default NotFound
